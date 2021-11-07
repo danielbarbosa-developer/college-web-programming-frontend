c46c47f38b826ffe95c21cc4b5315f4eebdb9361
danielbarbosa-developer <danielbarbosa.p@outlook.com>
2021-11-07 18:51:19 -0300

    [Version 1.0.0] Creating register and login validations and form submit

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,23 +1,36 @@
 import {FiLogIn, FiMail, FiLock} from 'react-icons/fi';
 import {Button} from "../../components/button";
 import {Link} from "react-router-dom";
-import{Container, FormContainer, InputContainer, Background, Content} from "./styles";
+import{Container, FormContainer, InputContainer, Error, Background, Content} from "./styles";
+import {useForm} from 'react-hook-form';
+
+interface FormData{
+    email: string;
+    password: string;
+}
 
 export function Login(){
+
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>();
+
+    const onSubmit = handleSubmit(data => alert(JSON.stringify(data)))
+
     return(
         <Container>
             <Content>
                 <FormContainer>
                     <h2>Sign In</h2>
-                    <form action="">
+                    <form onSubmit={onSubmit}>
                         <InputContainer>
                             <FiMail size={40}/>
-                            <input type="email" placeholder="E-mail" />
+                            <input type="email" placeholder="E-mail" { ... register("email", {required: true})} />
                         </InputContainer>
+                        {errors.email && <Error>Filling in the field is mandatory</ Error>}
                         <InputContainer>
                             <FiLock size={40}/>
-                            <input type="password" placeholder="password" />
+                            <input type="password" placeholder="password" { ... register("password", {required:true})}/>
                         </InputContainer>
+                        {errors.password && <Error>Filling in the field is mandatory</ Error>}
                         <Button type="submit">Login</Button>
                     </form>
                     <Link to="/register">
